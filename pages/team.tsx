faba0dc937581dc0164bacc0ab6943c17fc3e374
Amit Jakubowicz <jakubowicz.amit@gmail.com>
2021-03-22 08:19:21 +0100

    Start team page

--- pages/team.tsx
+++ pages/team.tsx
@@ -1,9 +1,35 @@
 import * as React from 'react'
+import {NextPageContext} from "next";
+import {getTeamMembers, TeamMember} from "../lib";
 
-const Team = () => {
+interface Props {
+    members: TeamMember[]
+}
+
+const Team = (props: Props) => {
     return <div>
         <h1>Our team</h1>
+        <ul>
+            {
+                props.members.map(member => (
+                    <li>
+                        {
+                            member.name
+                        }
+                    </li>
+                ))
+            }
+        </ul>
     </div>
 }
 
 export default Team
+
+export async function getStaticProps({ locale }) {
+    const team = getTeamMembers(locale.substr(0,2))
+    return {
+        props: {
+            members: team
+        }, // will be passed to the page component as props
+    }
+}
